3f7baf25b53f08acedd4d9a61179fc2184281616
xinnsin <xinshix09@gmail.com>
2026-06-10 21:36:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,21 @@ if [ -z "$VLESS_UUID" ]; then
 fi
 
 XRAY_PORT="${XRAY_PORT:-8080}"
+WS_PATH="${WS_PATH:-/ray}"
 
 sed \
   -e "s|\${VLESS_UUID}|${VLESS_UUID}|g" \
   -e "s|\${XRAY_PORT}|${XRAY_PORT}|g" \
+  -e "s|\${WS_PATH}|${WS_PATH}|g" \
   /etc/xray/config.template.json > /etc/xray/config.json
 
-echo "Starting Xray on port ${XRAY_PORT}"
-exec /usr/local/bin/xray run -config /etc/xray/config.json
+echo "Starting Xray on port ${XRAY_PORT}, WebSocket path ${WS_PATH}"
+/usr/local/bin/xray run -config /etc/xray/config.json &
+
+if [ -n "$TUNNEL_TOKEN" ]; then
+  echo "Starting Cloudflare Tunnel"
+  exec /usr/local/bin/cloudflared tunnel --no-autoupdate run --token "$TUNNEL_TOKEN"
+else
+  echo "TUNNEL_TOKEN is not set, only Xray started"
+  wait
+fi
